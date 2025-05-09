e20eef5b4318ac85275de103270dec0f7b831104
Vaggelis Magonezos <nospipian@gmail.com>
2025-05-09 19:44:25 +0300

    Update CustomMaterialAutocompleteSelect.tsx

--- app/traveler-details-form/CustomMaterialAutocompleteSelect.tsx
+++ app/traveler-details-form/CustomMaterialAutocompleteSelect.tsx
@@ -1,71 +1,3 @@
-// "use client";
-
-// import TextField from "@mui/material/TextField";
-// import Autocomplete from "@mui/material/Autocomplete";
-
-// //---------------------------------------------------------------------------
-
-// const CustomMaterialAutocompleteSelect = ({
-//   values,
-//   selected,
-//   label,
-//   set,
-//   error,
-// }: {
-//   values: Array<any>;
-//   selected: string;
-//   label: string;
-//   set: any;
-//   error: boolean;
-// }) => {
-//   const selectedValue = values.find((v) => v.value === selected) || "";
-//   return (
-//     <Autocomplete
-//       sx={{
-//         flex: 1,
-//         "& .MuiFormControl-root": {
-//           "&:hover": {
-//             backgroundColor: "white",
-//           },
-//           backgroundColor: "white",
-//           borderRadius: 0,
-//         },
-//         "& .MuiInputBase-root": {
-//           backgroundColor: "white",
-//         },
-//       }}
-//       autoComplete
-//       options={values}
-//       renderInput={(params) => {
-//         //params.inputProps.style = { fontSize: 13 };
-//         return (
-//           <TextField
-//             variant="filled"
-//             {...params}
-//             label={label}
-//             error={error}
-//             slotProps={{
-//               inputLabel: {
-//                 shrink: true,
-//               },
-//             }}
-//           />
-//         );
-//       }}
-//       onChange={(e, value) => {
-//         set(value?.value || "");
-//       }}
-//       getOptionLabel={(option) => {
-//         const label = values.find((v: any) => v.value === option.value);
-//         return label?.label || "";
-//       }}
-//       value={selectedValue}
-//     />
-//   );
-// };
-
-// export default CustomMaterialAutocompleteSelect;
-
 "use client";
 
 import { useState } from "react";
@@ -79,7 +11,12 @@ const CustomMaterialAutocompleteSelect = ({
   name, // Add name prop for form submission
   required = false,
 }: {
-  values: Array<{ label: string; value: unknown }>;
+  values: Array<{
+    label: string;
+    value: {
+      [key: string]: string | number | boolean | null | undefined;
+    };
+  }>;
   label: string;
   error?: boolean;
   name: string;
